237de2de2a662997dc8883f14a4355ae7cf42b63
Julien-Rtg <julien.rittl@gmail.com>
2024-05-18 17:34:25 +0200

    welcome

--- frontend/src/app/home/home.component.ts
+++ frontend/src/app/home/home.component.ts
@@ -10,4 +10,28 @@ import { RouterOutlet } from '@angular/router';
 })
 export class HomeComponent {
 
+  public title: string = 'WELCOME TO MY PORTFOLIO.';
+  public titleToShow: string = '';
+
+  public constructor() { 
+    this.titleAnimation();
+  }
+
+  public async titleAnimation(): Promise<void> {
+    const countTitle = this.title.length;
+    for (const i of Array(countTitle).keys()) {
+      setTimeout(() => {
+        // Remove the last character if it is an underscore
+        this.titleToShow.charAt(i) === '_' ? this.titleToShow = this.titleToShow.slice(0, -1) : null;
+        this.titleToShow += this.title[i];
+        if (i === countTitle - 1) {
+          setTimeout(() => {
+            this.titleToShow = '';
+            this.titleAnimation();
+          }, 3000);
+        }
+      }, 200 * i);
+    }
+  }
+
 }
